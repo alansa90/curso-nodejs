59b8dcfc920c58756679de18c4e63695831ed611
alansa90 <alkinsret@gmail.com>
2021-04-04 20:27:04 -0300

    .env config in dbConnection

--- newsPortal/config/dbConnection.js
+++ newsPortal/config/dbConnection.js
@@ -1,18 +1,10 @@
+require('dotenv/config')
 const mysql = require('mysql')
-const {
-  DB_HOST,
-  DB_USER,
-  DB_PASS,
-  DB_DATABASE,
-  DB_PORT
-} = require('./www')
-
-console.log(process.env.DB_HOST)
 
 module.exports = () => mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_DATABASE,
-  port: DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 })

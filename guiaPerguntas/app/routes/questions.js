e95a5fbb41c649d5f1efd6ae02c96cd9d395af2d
alansa90 <alkinsret@gmail.com>
2021-04-21 15:07:45 -0300

    partials nav

--- guiaPerguntas/app/routes/questions.js
+++ guiaPerguntas/app/routes/questions.js
@@ -2,4 +2,13 @@ module.exports = (application) => {
   application.get('/form', (req, res) => {
     res.render('questions/form')
   })
+
+  application.post('/save', (req, res) => {
+    const {
+      title,
+      description
+    } = req.body
+    res.send(`Title: ${title}\n
+      Description: ${description}`)
+  })
 }
--- guiaPerguntas/config/server.js
+++ guiaPerguntas/config/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.set('view engine', 'ejs')
 app.use(express.static('app/public'))
 app.set('views', './app/views')
+
 
 
 module.exports = app
